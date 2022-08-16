c15b862ab071c72cf4bc05ad63253e177975f1aa
David <davidspader97@gmail.com>
2022-08-15 21:24:48 -0300

    wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => 'David',
             'email' => 'davidspader97@gmail.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$CNFpAR.RI3vpJnQqCpfpdubxjEchUGrl5JuQRhVn7WpFpaTu2P7nq', // password
             'remember_token' => Str::random(10),
         ];
     }
