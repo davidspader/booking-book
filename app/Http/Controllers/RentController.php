d7acf19372f4807b5e0e06661456f3ede43b2fe5
David <davidspader97@gmail.com>
2022-08-26 21:44:17 -0300

    add houses to homepage

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -6,8 +6,6 @@ use App\Http\Requests\RentRequest;
 use App\Models\House;
 use App\Models\Rent;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 
